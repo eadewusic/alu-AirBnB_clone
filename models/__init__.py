b6e071976e236e17c2e1b219b470f4b4393f6c20
Eunice Adewusi <e.adewusi@alustudent.com>
2024-01-13 17:56:20 +0100

    update files

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,8 @@
-# models/__init__.py
 from models.engine.file_storage import FileStorage
 
+# Define BaseModel first.
+from models.base_model import BaseModel
+
+# Ensure that the storage instance is created after the BaseModel class has been defined.
 storage = FileStorage()
+storage.reload()
