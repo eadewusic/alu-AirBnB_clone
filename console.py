6e0adeed777189cb1716451dfe46f19910fe6b20
Eunice Adewusi <e.adewusi@alustudent.com>
2024-01-13 23:21:36 +0100

    make file executable

--- console.py
+++ console.py
@@ -7,22 +7,16 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """
-    HBNBCommand class inherits from cmd.Cmd and represents
-    the command interpreter for the HBNB project.
+    HBNBCommand class for the command interpreter.
     """
-
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """
-        Quit command to exit the program.
-        """
+        """Quit command to exit the program."""
         return True
 
     def do_EOF(self, arg):
-        """
-        Exit the program when encountering the end-of-file (EOF).
-        """
+        """EOF command to exit the program."""
         print()
         return True
 
