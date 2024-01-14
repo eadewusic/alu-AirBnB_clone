4278ea16d84d1f205ad8211cc83366166945e3e3
Eunice Adewusi <e.adewusi@alustudent.com>
2024-01-15 00:17:32 +0100

    update test_amenity.py file

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,18 +1,31 @@
-import datetime
+#!/usr/bin/python3
+'''Test class for the `Amenity` class
+'''
 import unittest
 from models.amenity import Amenity
+from models.base_model import BaseModel
 
-class TestAmenity(unittest.TestCase):
-    def test_attributes_types(self):
-        amenity = Amenity()
-        self.assertIsInstance(amenity.id, str)
-        self.assertIsInstance(amenity.created_at, datetime.datetime)  # Use datetime.datetime here
-        self.assertIsInstance(amenity.updated_at, datetime.datetime)  # Use datetime.datetime here
 
-    def test_amenity_custom_attribute(self):
-        amenity = Amenity()
-        amenity.category = "Entertainment"
-        self.assertEqual(amenity.category, "Entertainment")
+class TestAmenityClass(unittest.TestCase):
+    '''TestAmenityClass: defines tests for `Amenity` objects
+    '''
 
-if __name__ == '__main__':
+    def test_inherits_from_BaseModel(self):
+        '''Ensures that `Amenity` objects inherit from BaseModel
+        '''
+        amenity_instance = Amenity()
+        self.assertIsInstance(amenity_instance, BaseModel)
+
+    def test_has_name_attribute(self):
+        '''Ensures `Amenity` object has the attribute `name`
+        '''
+        self.assertTrue(hasattr(Amenity, 'name'))
+
+    def test_name_attribute_type_str(self):
+        '''Ensures the `name` attribute is of type str
+        '''
+        self.assertIsInstance(Amenity.name, str)
+
+
+if __name__ == "__main__":
     unittest.main()
