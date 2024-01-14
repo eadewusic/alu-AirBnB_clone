93941071a217b70e550adc6aa68c26c7e6c15649
EUNICE ADEWUSI (CLIMIRADI) <e.adewusi@alustudent.com>
2024-01-14 08:04:10 +0100

    Update console.py file

--- console.py
+++ console.py
@@ -5,17 +5,40 @@ This module contains the entry point for the command interpreter.
 
 import cmd
 
+
 class HBNBCommand(cmd.Cmd):
     """
     HBNBCommand class for the command interpreter.
+    
+    commands:
+    quit - exit the program
+    EOF - exit the program
     """
     prompt = "(hbnb) "
 
-    def do_quit(self, arg):
+    def do_help(self, arg: str) -> bool | None:
+        '''
+        Method to handle help command
+        '''
+        return super().do_help(arg)
+
+    def emptyline(self) -> bool:
+        '''
+        Method to handle empty line
+        '''
+        pass
+
+    def help_quit(self) -> None:
+        '''
+        Method to print help for quit command
+        '''
+        print("Quit command to exit the program")
+
+    def do_quit(self, arg: str) -> bool:
         """Quit command to exit the program."""
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, arg: str) -> bool:
         """EOF command to exit the program."""
         print()
         return True
