c9cc5458dfa9518fd4d7791379f488e00f4b081b
Eunice Adewusi <e.adewusi@alustudent.com>
2024-01-15 00:20:42 +0100

    update module test files

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,18 +1,51 @@
-import datetime
-import unittest
+#!/usr/bin/python3
+'''Defines tests for the `Review` class.
+'''
 from models.review import Review
+from models.base_model import BaseModel
+import unittest
 
-class TestReview(unittest.TestCase):
-    def test_attributes_types(self):
-        review = Review()
-        self.assertIsInstance(review.id, str)
-        self.assertIsInstance(review.created_at, datetime.datetime)
-        self.assertIsInstance(review.updated_at, datetime.datetime)
 
-    def test_review_custom_attribute(self):
-        review = Review()
-        review.rating = 5
-        self.assertEqual(review.rating, 5)
+class TestReviewClass(unittest.TestCase):
+    '''TestReviewClass: Defines tests for the `Review` class.
+    '''
 
-if __name__ == '__main__':
+    def test_inherits_from_BaseModel(self):
+        '''Test ensures `Review` inherits from BaseModel.
+        '''
+        test_review_instance = Review()
+        self.assertIsInstance(test_review_instance, BaseModel)
+
+    def test_has_place_id_attribute(self):
+        '''Test ensures `place_id` attribute exists in `Review` class.
+        '''
+        self.assertTrue(hasattr(Review, 'place_id'))
+
+    def test_place_id_attribute_type_str(self):
+        '''Test ensures `place_id` attribute in `Review` class is of type str.
+        '''
+        self.assertIsInstance(Review.place_id, str)
+
+    def test_has_user_id_attribute(self):
+        '''Test ensures `user_id` attribute exists in `Review` class.
+        '''
+        self.assertTrue(hasattr(Review, 'user_id'))
+
+    def test_user_id_attribute_type_str(self):
+        '''Test ensures `user_id` attribute in `Review` class is of type str.
+        '''
+        self.assertIsInstance(Review.user_id, str)
+
+    def test_has_text_attribute(self):
+        '''Test ensures `text` attribute exists in `Review` class.
+        '''
+        self.assertTrue(hasattr(Review, 'text'))
+
+    def test_text_attribute_type_str(self):
+        '''Test ensures `text` attribute in `Review` class is of type str.
+        '''
+        self.assertIsInstance(Review.text, str)
+
+
+if __name__ == "__main__":
     unittest.main()
