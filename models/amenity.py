2e1b5ea648998e71d62f41df23ce7dca6d17ed68
Eunice Adewusi <e.adewusi@alustudent.com>
2024-01-15 00:23:09 +0100

    update amenity.py file

--- models/amenity.py
+++ models/amenity.py
@@ -1,14 +1,7 @@
 # models/amenity.py
 from models.base_model import BaseModel
 
-class Amenity(BaseModel):
-    """
-    Amenity class for the HBNB project.
 
-    Attributes:
-        name (str): The name of the amenity.
-    """
-    def __init__(self, *args, **kwargs):
-        """Initialize Amenity instance."""
-        super().__init__(*args, **kwargs)
-        self.name = ""
+class Amenity(BaseModel):
+    """Amenity class for the HBNB project."""
+    name = ""
