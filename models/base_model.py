b63464bf75f7a2ace6b68d76f325c653566b9e3f
Eunice Adewusi <e.adewusi@alustudent.com>
2024-01-21 16:09:45 +0100

    resolving bugs

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
-from models import storage
+
 
 class BaseModel:
     """A base class for all hbnb models"""
@@ -30,6 +30,7 @@ class BaseModel:
 
     def save(self):
         """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
