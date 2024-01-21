5368ebd9c02f3ca3acd28f12798cbe7965e5d391
Eunice Adewusi <e.adewusi@alustudent.com>
2024-01-21 17:17:58 +0100

    resolving bugs

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,53 +1,49 @@
-import json
-import inspect
-import sys
-import os
-from models.base_model import BaseModel
+import unittest
+from models.engine.file_storage import FileStorage
 
-class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
-    __objects = {}
 
-    def all(self):
-        """Returns the dictionary __objects"""
-        return FileStorage.__objects.copy()
+class TestFileStorage(unittest.TestCase):
+    def test_all_method(self):
+        storage = FileStorage()
+        objects = storage.all()
+        self.assertIsInstance(objects, dict)
+        # Add more test cases specific to the FileStorage model
 
-    def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+    def test_new_method(self):
+        storage = FileStorage()
+        # Create a dummy object to test the new method
 
-    def delete(self, key):
-        """Deletes an object from __objects if it is inside"""
-        if key in FileStorage.__objects.keys():
-            del FileStorage.__objects[key]
-            self.save()
+        class DummyObject:
+            def __init__(self, id):
+                self.id = id
 
-    def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, "w", encoding='utf-8') as f:
-            casted_dict = {}
-            for key, value in FileStorage.__objects.items():
-                casted_dict[key] = value.to_dict()
-            json.dump(casted_dict, f)
+            def to_dict(self):
+                return {'id': self.id, '__class__': type(self).__name__}
 
-    def reload(self):
-        """Deserializes the JSON file to __objects"""
-        try:
-            with open(FileStorage.__file_path, "r", encoding='utf-8') as f:
-                data = json.load(f)
-                for key, value in data.items():
-                    class_name = value.get("__class__")
-                    if class_name:
-                        cls = globals().get(class_name)
-                        if cls and inspect.isclass(cls) and issubclass(cls, BaseModel):
-                            FileStorage.__objects[key] = cls(**value)
-        except FileNotFoundError:
-            pass
-        except json.JSONDecodeError:
-            pass
-        except NameError:
-            pass
-        except IOError:
-            pass
+        dummy_instance = DummyObject(id='dummy_id')
+        storage.new(dummy_instance)
+        self.assertIn('DummyObject.dummy_id', storage.all())
+
+    def test_save_and_reload_methods(self):
+        storage = FileStorage()
+        # Create a dummy object to test save and reload methods
+
+        class DummyObject:
+            def __init__(self, id):
+                self.id = id
+
+            def to_dict(self):
+                return {'id': self.id, '__class__': type(self).__name__}
+
+        dummy_instance = DummyObject(id='dummy_id')
+        storage.new(dummy_instance)
+        storage.save()
+
+        # Create a new storage instance to test the reload method
+        new_storage = FileStorage()
+        new_storage.reload()
+        self.assertIn('DummyObject.dummy_id', new_storage.all())
+
+
+if __name__ == '__main__':
+    unittest.main()
