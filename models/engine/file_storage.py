8e8755e250aa156b3065e07f93c7285648ed1d7c
Eunice Adewusi <e.adewusi@alustudent.com>
2024-01-21 17:05:30 +0100

    resolving bugs

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,8 +1,9 @@
-import json
+#!/usr/bin/python3
+"""This module defines a class to manage file storage for hbnb clone"""
+from models import base_model, user, place, state, city, amenity, review
 import inspect
+import json
 import sys
-import os
-from models.base_model import BaseModel
 
 
 class FileStorage:
@@ -11,45 +12,34 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects"""
-        return FileStorage.__objects.copy()
+        """Returns a dictionary of models currently in storage"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
-
-    def delete(self, key):
-        """Deletes an object from __objects if it is inside"""
-        if key in FileStorage.__objects.keys():
-            del FileStorage.__objects[key]
-            self.save()
+        """Adds new object to storage dictionary"""
+        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)"""
-        with open(FileStorage.__file_path, "w", encoding='utf-8') as f:
-            casted_dict = {}
-            for key, value in FileStorage.__objects.items():
-                casted_dict[key] = value.to_dict()
-            json.dump(casted_dict, f)
+        """Saves storage dictionary to file"""
+        with open(FileStorage.__file_path, 'w') as f:
+            temp = {}
+            temp.update(FileStorage.__objects)
+            for key, val in temp.items():
+                temp[key] = val.to_dict()
+            json.dump(temp, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects"""
+        """Loads storage dictionary from file"""
+        classes = {cls.__name__: cls for name, cls in inspect.getmembers(
+            sys.modules[__name__], inspect.isclass)}
+
         try:
-            with open(FileStorage.__file_path, "r", encoding='utf-8') as f:
-                data = json.load(f)
-                for key, value in data.items():
-                    class_name = value.get("__class__")
-                    if class_name:
-                        cls = globals().get(class_name)
-                        if cls and inspect.isclass(
-                                cls) and issubclass(cls, BaseModel):
-                            FileStorage.__objects[key] = cls(**value)
+            temp = {}
+            with open(FileStorage.__file_path, 'r') as f:
+                temp = json.load(f)
+                for key, val in temp.items():
+                    class_name = val['__class__']
+                    if class_name in classes:
+                        self.all()[key] = classes[class_name](**val)
         except FileNotFoundError:
-            pass
-        except json.JSONDecodeError:
-            pass
-        except NameError:
-            pass
-        except IOError:
             pass
